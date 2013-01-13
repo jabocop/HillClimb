bc8fe4292b3416022621593f03311937b2126598
Jacob Ankarberg <jacob@ankarberg.net>
2013-01-13 21:25:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HillClimbHighscore::Application.config.secret_token = 'd943d997c0ffe087e1ca5f852a9f2d228a4bcf248ccf940870d061e7b6453f4a3d9445330845b91712a2017a330fd77992657ff470101d2dd5f1f3679ee78d53'

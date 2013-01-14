9122a8e9b16dad4332a350292fe09ea571ebbce8
Jacob Ankarberg <jacob@ankarberg.net>
2013-01-14 23:35:26 +0100

    Added some unit tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  it "should have the content 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', text: 'Hill climb racer')
-    end
+  subject {page}
+  describe "Home page" do
+    before {visit root_path}
+    
+    it { should have_selector('h1',text: 'Hill climb racer') }
+    it { should have_selector('title', text: 'Hill Climb Highscore') }
+    it { should_not have_selector('title', text: '|')}
+    
+  end
+  
+  describe "About Page" do
+    before {visit about_path}
+    
+    it {should have_selector('h1',text: 'About')}
+    it { should have_selector('title', text: 'Hill Climb Highscore | About') }
+    
+  
+  end  
 end

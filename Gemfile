3028e31dc89b47007d6cd74ca31a844bbc63c2ed
Jacob Ankarberg <jacob@ankarberg.net>
2013-01-14 23:18:28 +0100

    Added basic structure

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
   gem 'capybara', '1.1.2'
+  
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+  it "should have the content 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', text: 'Hill climb racer')
     end
-  end
 end
